69f7a197359d6fbce9a42a793f427499a2206c21
BludosDev <bludosofficial+67@gmail.com>
2026-02-13 01:37:04 +0530

    Initial commit

---
+++ src/main/java/com/bludos/absentecho/Main.java
@@ -0,0 +1,10 @@
+package com.bludos.absentecho;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("absent_echo")
+public class Main {
+    public Main() {
+        System.out.println("Hello from Project Void-Self!");
+    }
+}
